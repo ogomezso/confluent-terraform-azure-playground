a0de686adabd5e07f4d62be3c221408b27a44b73
Oscar Gomez Soriano <ogomezso0@gmail.com>
2024-04-10 13:51:32 +0100

    tf backend

---
+++ private-link/confluent/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "ogomezso"
+    storage_account_name = "ogomezsostorage"
+    container_name       = "pltfstate"
+    key                  = "terraform.tfstate"
+  }
+}
