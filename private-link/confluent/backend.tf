f1dca280b5c23f4374300428f3111f619ce75a76
Oscar Gomez Soriano <ogomezso0@gmail.com>
2024-04-10 16:30:01 +0100

    azure provider auth test

--- private-link/confluent/backend.tf
+++ private-link/confluent/backend.tf
@@ -4,6 +4,6 @@ terraform {
     storage_account_name = "ogomezsostorage"
     container_name       = "pltfstate"
     key                  = "terraform.tfstate"
-    use_msi              = true
+    use_oidc              = true
   }
 }
