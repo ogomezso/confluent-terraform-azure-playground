1e99243a88efaea16c71c1c0c38cfb18ef272e96
Oscar Gómez Soriano <ogomezso0@gmail.com>
2024-09-18 11:56:43 +0100

    Update azure provider info

--- private-link/confluent/main.tf
+++ private-link/confluent/main.tf
@@ -89,12 +89,7 @@ resource "confluent_api_key" "app-manager-kafka-api-key" {
 }
 
 provider "azurerm" {
-  features {
-  }
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
+  features {}
 }
 
 locals {
